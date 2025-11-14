e939e96477af79e475ce116147fa38e953324b5e
USUÁRIO <USURIO@DESKTOP-DETP0L9>
2025-11-14 13:30:24 -0300

    Adicionar arquivos de projeto.

---
+++ test/CursoOnline.DominioTest/Cursos/CursoTest_DominioCurso.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//AULA 'Domínio Curso'
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTest_DominioCurso
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            const string nome = "Engenharia da Computação";
+            const double cargaHoraria = 800;
+            const string publicoAlvo = "Estudante";
+            const double valor = 895;
+            
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(valor, curso.Valor);
+        }
+
+        public class Curso
+        {
+            public string Nome { get; private set; }
+            public double CargaHoraria { get; private set; }
+            public string PublicoAlvo { get; private set; }
+            public double Valor { get; private set; }
+
+            public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+            {
+                Nome = nome;
+                CargaHoraria = cargaHoraria;
+                PublicoAlvo = publicoAlvo;
+                Valor = valor;
+            }
+        }
+    }
+}
---
+++ test/CursoOnline.DominioTest/MeuPrimeiroTeste.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoOnline.DominioTest
+{
+    public class MeuPrimeiroTeste
+    {
+        [Fact(DisplayName = "Apelido Teste")]
+        public void DeveVariavel1SerIgualVariavel2()
+        {
+            //Arrange - Preparação do Cenário
+            var variavel1 = 1;
+            var variavel2 = 2;
+
+            //Act - Ação
+            //Exemplo
+            //variavel1 = variavel2;
+
+            //Assert - Verificação
+            Assert.Equal(variavel1, variavel2);
+
+        }
+    }
+}
